350f1dae8d54d6f117ccf05097e711dead357422
Samir Parhi <samirparhi@gmail.com>
2023-12-15 10:38:41 +0530

    updated

--- tools/blobsign-cosign.sh
+++ tools/blobsign-cosign.sh
@@ -2,38 +2,44 @@
 
 set -e
 
-echo "Important !!! This script only sign the file/Blobs"
+echo -e "\n Important !!! This script only sign the file/Blobs"
 
-echo "Please keep this script in the same folder where your file is located which intended to be signed"
+echo -e "\n Please keep this script in the same folder where your file is located which intended to be signed"
 
-echo "Chose the Name for the Private and Public Key e.g(my-key)"
+echo -e "\n Provide the Name for the Private and Public Key e.g(my-key)"
 
 read keyName
 
-echo "Generating the Private and Public Key in Cosign..."
+echo -e "\n Generating the Private and Public Key in Cosign..."
 
 cosign generate-key-pair --output-key-prefix $keyName
 
-echo "Enter the File Name which has to be signed"
+echo -e "\n Enter the File Name which has to be signed"
 
 read fileName
 
-echo "Enter the Output signature file Name"
+echo -e "\n Enter the Output signature file Name"
 
 read signatureFileName
 
-echo " Now Sign the Given File Using Private Key e.g. ($keyName.key)"
+echo -e "\n Signing the Given File Using Private Key e.g. ($keyName.key)"
 
 cosign sign-blob $fileName --key $keyName.key -output-signature $signatureFileName
 
 signText= cat $signatureFileName
 
-echo "your signature is: $signText"
+echo -e "\n your signature is: $signText"
 
-echo " Would you like to Verify the Signature Now ? Y = Yes, N = No"
+echo -e " Would you like to Verify the Signature Now ? Y = Yes, N = No"
+
 read option
+
 if [ $option -eq "Y" ]
-echo " Verifying the Sign of $fileName Using Public Key e.g. ($keyName.pub)"
+
+echo " \n Verifying the Sign of $fileName Using Public Key e.g. ($keyName.pub)"
 
 cosign verify-blob $fileName --key $keyName.pub --signature $signatureFileName
+
 fi
+
+echo -e "\n  ✅ Process Ends Successfully "
